dd5a08b0f272040d5091577747d9b678469b1cb1
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-07-16 13:56:39 -0300

    Projeto em desenvolvimento

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -43,18 +43,20 @@ export const Body = () => {
         const [input, setInput] = useState<number>(1);
 
         const pokemon = (e : any) => {
-              
+          e.preventDefault();
              if (input < 1) {
                 setInput(1);
 
+            } else if(!e){
+              alert("Por favor escreva um número para consultar a api.")
+              return
             }
-            else{
-                setInput(e);
-            } 
+             
 
             fetch(`https://pokeapi.co/api/v2/pokemon/${input}/`)
             .then(response => response.json())
             .then((json) => {
+
                 setArray([
 
                     {   ...json,
@@ -66,36 +68,66 @@ export const Body = () => {
                 ])
             })
         }
+
+        const handleKeyPress  = (e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === 'Enter') {
+                pokemon(e);
+              }
+        }
         
        return (
     <div>
-      <div className='flex justify-center p-4 border-b'>
+      <div className='flex flex-row md:flex-row justify-center 
+                      p-4 border-b space-y-3 md:space-y-0
+                      '>
         <input
           type='number'
-          className='text-black p-2 rounded-full'
+          className='text-black md:p-2 p-1 rounded-full
+                         w-48 mt-2 md:mt-0 pl-3'
           value={input}
           onChange={(e) => setInput(parseInt(e.target.value))}
           placeholder='Escreva um número'
+          onKeyPress={handleKeyPress}
+
         />
         <button
-          className='ml-3 bg-sky-600 p-2 rounded-full hover:bg-sky-400'
+          className='ml-3 bg-sky-600 md:p-2 p-2 
+                    rounded-full hover:bg-sky-400
+                     w-32
+                    '
           onClick={pokemon}
+
         >
           Consultar api
         </button>
       </div>
       <section className='flex justify-center'>
-        <div className=''>
-          {array.length <= 0 && (
-            <p className=''>
-              Escreva um número no input acima para aparecer um pokemon.
-            </p>
-          )}
-        </div>
 
-        <div className='flex justify-center m-3 bg-slate-700 w-96 rounded-[20px]'>
-          {array.map((val) => {
+        <div className={ array.length <= 0? 
+                        'flex justify-center m-3'
+
+                        :
+                        'flex justify-center m-3 bg-slate-700 w-96 rounded-[20px]'
+
+                      }>
+          
+
+              {
+              array.length <= 0? (
+                <div>
+                  <p className=''>
+                  Escreva um número no input acima para aparecer um pokemon. 
+                  
+                </p>
+
+                </div>
+              )
+
+              :
+
+            array.map((val) => {
             return (
+
               <div key={val.species.name}>
                 <p className='text-center m-2 text-2xl md:text-3xl border-b pb-3'>
                   {val.species.name}
